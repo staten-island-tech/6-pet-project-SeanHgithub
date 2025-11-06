fb5cbb5c3d730679880c747014bfefe8536e6e93
SeanHgithub <seanhadley11@gmail.com>
2025-11-06 10:16:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ Calculator.add_many([4,32,6,9,214]) """
 Sean = Hero("Sean", 600, ["Sword"])
 Sean.buy("Axe")
 print(Sean.__dict__) """
+import random
 class Dog:
     def __init__(self, name, happiness, hunger, energetic, dirty, day):
         self.name = name
@@ -71,23 +72,19 @@ class Dog:
         self.energetic += energy
         day += 1
 
-    def hunger(self, cals, food):
-        self.food = food
+    def hunger(self):
         y = input(f"What do you want to feed {self.name}?(chicken, peanut butter, bread, crackers)")
-        self.cals = cals
         if y == "chicken":
-            cals = 100
+            self.hunger += random.randint(60,80)
         elif y == "peanut butter":
-            cals = 75
+            self.hunger += random.randint(40,60)
         elif y == "bread":
-            cals = 50
+            self.hunger += random.randint(20,40) 
         elif y == "cracker":
-            cals = 25
+            self.hunger += random.randint(1,20)
         else:
             print(f"invalid action, you won't be able to feed {self.name} again until tomorrow(sorry)")
-            self.food = y
-        self.hunger += cals
-        print(f"you fed {self.name} {self.food} and her hunger is now {self.hunger}")
+        print(f"you fed {self.name} {y} and her hunger is now {self.hunger}")
     
     
     
@@ -121,16 +118,22 @@ class Dog:
     def Day_1(self):
         y = False
         while y == False:
-            y = input(f"where will {self.name} sleep?(bed, chair, floor, lawn)")
+            y = input(f"{self.name} is exhausted and cold, where will she sleep today?(bed, chair, floor, lawn)")
             if y == "bed":
+                self.energetic += random.randint(60,80)
                 print(f"{self.name} slept very good, great job.")
                 y = True
+                self.Day_2()
             elif y == "chair":
                 print(f"{self.name} slept well, good job.")
+                self.energetic += random.randint(40,60)
                 y = True
+                self.Day_2()
             elif y == "floor":
                 print(f"{self.name} slept ok, but if it keeps up, there will be consequences.")
                 y = True
+                self.energetic += random.randint(20,40)
+                self.Day_2()
             elif y == "lawn":
                 self.death("Freezed")
                 y = True
@@ -138,22 +141,25 @@ class Dog:
                 print("Invalid response, try again. (bed, chair, floor, lawn)")
         
         y = False
-                
+    def Day_2(self):
+        print(f".....Day 2 begins. {self.name} is starving, get her something to eat.")
+        self.hunger()
+    
     def check_win(self):
         if self.day == 20 and self.alive == True:
             print(f"{self.name} survived for 2 weeks and is ready to go to the vet. CONGRATULATIONS!")
-    def background(self):
-        story = input("You found a stray dog in the freezing rain and took it in. You must foster her over the next two weeks by feeding, cleaning, and playing with the dog, as well as giving it a place to sleep. Do you accept this challenge?")
-        if story.lower == "yes":
-            print("........Boom!, the door slams behind you as you bring her in, day 1 begins......")
-        elif story.lower == "no":
-            print("Ok, if you must.")
-        else:
-            print("Sorry, try again.(yes/no)")
-            input(story)
 
 
 
-x = input("What do you want to name your new dog?")
-x = Dog(f"{x}", 0, 0, 0, 0, 0)
-x.Day_1()
+
+story = input("You found a stray dog in the freezing rain and took it in. You must foster her over the next two weeks by feeding, cleaning, and playing with the dog, as well as giving it a place to sleep. Do you accept this challenge?")
+if story == "yes":
+    print("........Boom!, the door slams behind you as you bring her in, day 1 begins......")
+    x = input("What do you want to name your new dog?")
+    x = Dog(f"{x}", 0, 0, 0, 0, 0)
+    x.Day_1()
+elif story == "no":
+    print("Ok, if you must.")
+else:
+    print("Sorry, try again.(yes/no)")
+
