fd2ec5876ef93fc3c1605998978000e3c5a9b172
SeanHgithub <seanhadley11@gmail.com>
2025-11-19 10:03:47 -0500

    Update password.py

--- password.py
+++ password.py
@@ -1,11 +1,19 @@
 def isValid(email, password):
     if  "@" not in email or type(email) != str: return "not valid email"
+    upper = 0
+    number = 0
     if type(password) != str:
         return "not a valid password"
     if len(password) < 8:  
         return "password is not long enough"
-    if not password.isupper() > 0: 
+    for char in password:
+        if char.isupper():
+            upper +=1
+        if char.isdigit():
+            number += 1
+    if upper == 0:
         return "the password must contain atleast 1 uppercase letter."
-    if not password.isdigit() > 0: 
+    if number == 0:
         return "the password must contain atleast 1 number."
-print(isValid("Seanhadley11@gmail.com", "Hello123"))
+    return(email, password)
+print(isValid("Seanhadley11@gmail.com", "helLo"))
